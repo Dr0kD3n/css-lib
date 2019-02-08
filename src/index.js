096e6978362faa858b145eec748d66dc3383a056
Dr0kD3n <bkerikbk@gmail.com>
2019-02-08 23:10:03 +0300

    timeline colors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-import 'bootstrap'
 import './sass/main.scss'
 
+const animationDuration = 300;
+
+$(document).ready(() => {
+    const popup = (text, className, timeline = false, duration = 1000) => {
+        const hash = btoa(Math.random()).replace(/=/g, '');
+        let el = null;
+        const content = `${text} <div class='time-line ${!timeline && 'hidden'}'></div>`
+        $('body').append(`
+        <div class='message ${hash} ${className}'>${content}
+        </div>
+        `)
+            .promise()
+            .then(() => {
+                el = $(`.message.${hash}`);
+                el.animate({
+                    left: `-=${el.width() + 10}px`
+                }, 100)
+            })
+            .then(() =>
+                $(`.message.${hash} .time-line`)
+                    .eq(0)
+                    .animate({ 'width': '0' }, duration, () =>
+                        el.animate({
+                            marginTop: `-${el.height()}px`,
+                            opacity: '0'
+                        }, animationDuration)
+                    ))
+        el && el.on('click', () =>
+            el.animate({
+                marginTop: `${el.height()}px`,
+                opacity: '0'
+            }, animationDuration))
+    }
+    popup('123', 'success', true)
+    popup('123', 'primary')
+    popup('123123123123132131123123112312312312313213112312311231231231231321311231231', 'warning')
+    popup('123', 'danger')
+})
---
+++ src/script.js
@@ -0,0 +1,4 @@
+
+const popup = (text) => {
+    console.log(text);
+}
