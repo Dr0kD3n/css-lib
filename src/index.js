ced3516586febee0710ad337bc044d5bb7beeb45
Dr0kD3n <bkerikbk@gmail.com>
2019-02-09 12:41:13 +0300

    tag

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import './sass/main.scss'
-import { tag } from 'postcss-selector-parser';
 
-const animationDuration = 300;
-
-$(document).ready(() => {
-    const checkInputs = () => {
-        $('input').map(e => {
-            const input = $('input').eq(e)[0];
-            $(input.nextElementSibling).css({
-                top: input.value ? '0' : '20px',
-                opacity: '1',
-                fontSize: input.value ? '12px' : 'unset',
-            })
+const animationDuration = 500;
+const checkInputs = () => {
+    $('input').map(e => {
+        const input = $('input').eq(e)[0];
+        $(input.nextElementSibling).css({
+            top: input.value ? '0' : '20px',
+            opacity: '1',
+            fontSize: input.value ? '12px' : 'unset',
         })
-    }
+    })
+}
+$(document).ready(() => {
+
     checkInputs()
     $('input').on('keyup', (e) => {
         const input = $(e.target).eq(0)[0];
@@ -39,21 +38,20 @@ $(document).ready(() => {
     const popup = (text, className, timeline = false, duration = 1000) => {
         const hash = btoa(Math.random()).replace(/=/g, '');
         let el = null;
-        const content = `${text} <div class='time-line ${!timeline && 'hidden'}'></div>`
-        $('body').append(`<div class='message ${hash} ${className}'>${content}</div>`)
+        const content = `${text} <div class='time-line ${!timeline && 'hidden'}'></div>`;
+        $('.messageBox').eq(0)[0] || $('body').append('<div class="messageBox"></div>');
+        $('.messageBox').append(`<div class='message ${hash} ${className}'>${content}</div>`)
             .promise()
             .then(() => el = $(`.message.${hash}`))
-            .then(() => {
-                el.animate({
-                    left: `-=${el.width() + 10}px`
-                }, 100)
-            })
+            .then(() => el.animate({
+                left: `-=${el.width() + 10}px`
+            }, 100))
             .then(() =>
                 $(`.message.${hash} .time-line`)
                     .eq(0)
                     .animate({ 'width': '0' }, duration, () =>
                         el.animate({
-                            marginLeft: `${el.width() + 20}px`,
+                            marginRight: `-${el.width() + 20}px`,
                         }, animationDuration, () => {
                             el.css('display', 'none')
                         })
@@ -61,11 +59,32 @@ $(document).ready(() => {
         $(`.message.${hash}`).eq(0).on('click', () => {
             el = $(`.message.${hash}`);
             el.animate({
-                marginLeft: `${el.width() + 20}px`,
+                left: `${el.width() + 20}px`,
             }, animationDuration, () =>
                     el.eq(0).css('display', 'none'))
         })
     }
-    popup('123')
-    popup('123', 'primary')
+    const tag = () => {
+        const hash = btoa(Math.random()).replace(/=/g, '');
+        const list = $(`.tag`);
+        list.each(e => {
+            const el = $(list).eq(e);
+            el.prepend('<div class="connector"></div>')
+            const parent = $($(el).eq(0)[0].parentElement);
+            $('.tag').eq(e).addClass(hash);
+            console.log(el.css('background-color'))
+            console.log()
+            el.css({
+                height: parent.height(),
+            })
+            parent.css({
+                border: `1px solid ${el.css('background-color')}`,
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingLeft: '10px';
+            })
+        });
+    }
+
 })
