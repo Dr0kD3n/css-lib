7477fa2985d31175957925e80a4421853cb68407
Dr0kD3n <bkerikbk@gmail.com>
2019-02-11 22:50:42 +0300

    navbar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './sass/main.scss'
+import './scss/main.scss'
+import variables from './scss/_variables.scss'
 
 const animationDuration = 500;
 const checkInputs = () => {
@@ -16,57 +17,91 @@ const dropdown = () => {
         const hash = `hash${btoa(Math.random()).replace(/=/g, '')}`;
         const el = $('.dropdown').eq(e);
         const header = el.find('.header').eq(0);
-        if (el.hasClass('center')) {
-            header.prepend('<div></div>');
-        }
-        header.append("<i class='fa fa-angle-down'></i>")
+        const inNav = el.parent().parent().has('.nav');
         const content = el.find('.content').eq(0);
-        header.on('click', () => {
-            content.animate({
-                height: 'toggle'
-            }, animationDuration)
-            header.find('.fa.rotated').eq(0)[0] ?
-                $(header.find('.fa')).removeClass('rotated').addClass('rotate') :
-                $(header.find('.fa')).removeClass('rotate').addClass('rotated')
-
-
-
-        })
         el.addClass(hash);
+        el.hasClass('center') && !inNav && header.html(`<div></div><div class="text">${header.html()}</div>`);
+        if (inNav) {
+            el.on('mouseenter', (e) => content.slideDown(animationDuration));
+            el.offset().left - $(window).width() < 0 && content.css('right', 0);
+            $(document).on('mousemove', (e) => {
+                const { className } = $(e).eq(0)[0].target;
+                const condition = className.includes('header') || className.includes('dropdown-item')
+                !condition && content.slideUp(animationDuration);
+            })
+        } else {
+            header.append("<i class='fa fa-angle-down'></i>")
+            header.on('click', () => {
+                content.animate({
+                    height: 'toggle'
+                }, animationDuration)
+                header.find('.fa.rotated').eq(0)[0] ?
+                    $(header.find('.fa')).removeClass('rotated').addClass('rotate') :
+                    $(header.find('.fa')).removeClass('rotate').addClass('rotated')
+            })
+        }
+
+    })
+}
+const popup = (text, className, timeline = false, duration = 1000) => {
+    const hash = btoa(Math.random()).replace(/=/g, '');
+    let el = null;
+    const content = `${text} <div class='time-line ${!timeline && 'hidden'}'></div>`;
+    $('.message-box').eq(0)[0] || $('body').append('<div class="message-box"></div>');
+    $('.message-box').append(`<div class='message ${hash} ${className}'>${content}</div>`)
+        .promise()
+        .then(() => el = $(`.message.${hash}`))
+        .then(() => el.animate({
+            left: `-=${el.width() + 10}px`
+        }, 100))
+        .then(() =>
+            $(`.message.${hash} .time-line`)
+                .eq(0)
+                .animate({ 'width': '0' }, duration, () =>
+                    el.animate({
+                        marginRight: `-${el.width() + 20}px`,
+                    }, animationDuration, () => {
+                        el.css('display', 'none')
+                    })
+                ))
+    $(`.message.${hash}`).eq(0).on('click', () => {
+        el = $(`.message.${hash}`);
+        el.animate({
+            left: `${el.width() + 20}px`,
+        }, animationDuration, () =>
+                el.eq(0).css('display', 'none'))
+    })
+}
+const dropdownGroup = () => {
+    $('.dropdown-group').each(e => {
+        const dropdownGroup = $('.dropdown-group').eq(e);
+        const dropdown = dropdownGroup.find('.dropdown');
+        dropdown.each(e => {
+            const hash = `hash${btoa(Math.random()).replace(/=/g, '')}`;
+            Object.values($(dropdown).eq(e)[0].classList)
+                .filter(e => e.includes('hash'))[0] ||
+                $(dropdown).eq(e).addClass(hash)
+        });
+        const header = dropdown.find('.header');
+        header.off('click').on('click', e => {
+            const content = dropdown.find('.content')
+            const el = $(e.target.parentElement);
+            let result = false;
+            dropdown.each(e => {
+                const hash = Object.values(el.eq(0)[0].classList).filter(e => e.includes('hash'))[0];
+                if (dropdown.eq(e).hasClass(hash)) {
+                    dropdown.eq(e).find('.content').css('display') === 'none' ?
+                        content.eq(e).slideDown(animationDuration) :
+                        content.eq(e).slideUp(animationDuration);
+                } else {
+                    content.eq(e).slideUp(animationDuration);
+                }
+            })
+        })
     })
 }
 $(document).ready(() => {
-    const dropdownGroup = () => {
-        $('.dropdown-group').each(e => {
-            const dropdownGroup = $('.dropdown-group').eq(e);
-            const dropdown = dropdownGroup.find('.dropdown');
-            dropdown.each(e => {
-                const hash = `hash${btoa(Math.random()).replace(/=/g, '')}`;
-                Object.values($(dropdown).eq(e)[0].classList)
-                    .filter(e => e.includes('hash'))[0] ||
-                    $(dropdown).eq(e).addClass(hash)
-            });
-            const header = dropdown.find('.header');
-            header.off('click').on('click', e => {
-                const content = dropdown.find('.content')
-                const el = $(e.target.parentElement);
-                let result = false;
-                dropdown.each(e => {
-                    const hash = Object.values(el.eq(0)[0].classList).filter(e => e.includes('hash'))[0];
-                    if (dropdown.eq(e).hasClass(hash)) {
-                        dropdown.eq(e).find('.content').css('display') === 'none' ?
-                            content.eq(e).slideDown(animationDuration) :
-                            content.eq(e).slideUp(animationDuration);
-                    } else {
-                        content.eq(e).slideUp(animationDuration);
-                    }
-                })
-            })
-        })
-    }
-    dropdown()
-    dropdownGroup()
-    checkInputs()
+
     $('input').on('keyup', (e) => {
         const input = $(e.target).eq(0)[0];
         const condition = input.value || $(e.target).eq(0).is(':focus');
@@ -89,35 +124,8 @@ $(document).ready(() => {
         if (tagName !== 'INPUT') checkInputs()
     })
 
-    const popup = (text, className, timeline = false, duration = 1000) => {
-        const hash = btoa(Math.random()).replace(/=/g, '');
-        let el = null;
-        const content = `${text} <div class='time-line ${!timeline && 'hidden'}'></div>`;
-        $('.message-box').eq(0)[0] || $('body').append('<div class="message-box"></div>');
-        $('.message-box').append(`<div class='message ${hash} ${className}'>${content}</div>`)
-            .promise()
-            .then(() => el = $(`.message.${hash}`))
-            .then(() => el.animate({
-                left: `-=${el.width() + 10}px`
-            }, 100))
-            .then(() =>
-                $(`.message.${hash} .time-line`)
-                    .eq(0)
-                    .animate({ 'width': '0' }, duration, () =>
-                        el.animate({
-                            marginRight: `-${el.width() + 20}px`,
-                        }, animationDuration, () => {
-                            el.css('display', 'none')
-                        })
-                    ))
-        $(`.message.${hash}`).eq(0).on('click', () => {
-            el = $(`.message.${hash}`);
-            el.animate({
-                left: `${el.width() + 20}px`,
-            }, animationDuration, () =>
-                    el.eq(0).css('display', 'none'))
-        })
-    }
+
+
     const tag = () => {
         const hash = btoa(Math.random()).replace(/=/g, '');
         const list = $(`.tag`);
@@ -140,5 +148,7 @@ $(document).ready(() => {
             })
         });
     }
-
+    dropdown()
+    dropdownGroup()
+    checkInputs()
 })
